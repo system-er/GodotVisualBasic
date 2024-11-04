d12c73bab9d5d4e5a4f9ad13acaec1d1c14b9e59
systemerror <sys_temerror@web.de>
2024-11-04 17:55:37 +0100

    Add files via upload

--- node2d.cs
+++ node2d.cs
@@ -1,21 +1,23 @@
 
-
 using Godot;
 using System;
 using vblibrary;
 
 public partial class node2d : Node2D
 {
-	Class1 vbclass = new Class1();
+	Class1 vbclass;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-		GD.Print("hello world from csharp");
-		vbclass._Ready();
+        GD.Print("hello world from csharp");
+        vbclass = new Class1(this);
+        vbclass._Ready();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		vbclass._Process();
 	}
 }
