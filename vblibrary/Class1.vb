e228c0fa782fd10986397c8167c2cba1ecd17bf0
systemerror <sys_temerror@web.de>
2024-11-04 17:56:02 +0100

    Add files via upload

--- vblibrary/Class1.vb
+++ vblibrary/Class1.vb
@@ -1,7 +1,19 @@
 Imports Godot
 
 Public Class Class1
-    Public Sub _Ready()
-        GD.Print("hello world from visualbasic")
+
+    Private node2d As Node2D
+
+    Public Sub New(n As Node2D)
+        node2d = n
     End Sub
+
+    Public Sub _Ready()
+        GD.Print("hello world from visualbasic from node ", node2d.Name)
+    End Sub
+
+    Public Sub _Process()
+
+    End Sub
+
 End Class
